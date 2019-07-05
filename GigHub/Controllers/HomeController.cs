7bfed3822c672ddc0fdb7446687f8c7cd0d85711
hasanslayer <hasan-dr2010@hotmail.com>
2019-07-05 02:05:57 +0200

    Add name to artist.

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using GigHub.Models;
 
 namespace GigHub.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upComingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Where(g => g.DateTime > DateTime.Now);
+            return View(upComingGigs);
         }
 
         public ActionResult About()
