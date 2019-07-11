5c1afeec438f81736b5a6e136dbb866285f51eec
hasanslayer <hasan-dr2010@hotmail.com>
2019-07-11 17:00:08 +0200

    Code refactoring

--- GigHub/Controllers/API/GigsController.cs
+++ GigHub/Controllers/API/GigsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -22,38 +23,17 @@ namespace GigHub.Controllers.API
         public IHttpActionResult Cancel(int id)
         {
             var userId = User.Identity.GetUserId();
-            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+            var gig = _context.Gigs
+                .Include(g => g.Attendances.Select(a => a.Attendee))
+                .Single(g => g.Id == id && g.ArtistId == userId);
 
             if (gig.IsCanceled)
             {
                 return NotFound();
             }
 
-            gig.IsCanceled = true;
+            gig.Cancel();
 
-            Notification notification = new Notification()
-            {
-                DateTime = DateTime.Now,
-                Gig = gig,
-                Type = NotificationType.GigCanceled
-            };
-
-            var attendees = _context.Attendances
-                .Where(a => a.GigId == gig.Id)
-                .Select(a => a.Attendee)
-                .ToList();
-
-            foreach (var attendee in attendees)
-            {
-                var userNotification = new UserNotification()
-                {
-                    User = attendee,
-                    Notification = notification
-                };
-
-                _context.UserNotifications.Add(userNotification);
-
-            }
 
             _context.SaveChanges();
 
--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace GigHub.Models
 {
@@ -7,7 +10,7 @@ namespace GigHub.Models
     {
         public int Id { get; set; }
 
-        public bool IsCanceled { get; set; }
+        public bool IsCanceled { get; private set; }
 
         public ApplicationUser Artist { get; set; }
 
@@ -25,7 +28,37 @@ namespace GigHub.Models
         [Required]
         public byte GenreId { get; set; }
 
+        public ICollection<Attendance> Attendances { get; private set; }
 
+        public Gig()
+        {
+            Attendances = new Collection<Attendance>();
+        }
 
+        public void Cancel()
+        {
+            IsCanceled = true;
+
+            Notification notification = Notification.GigCanceled(this);
+
+            foreach (var attendee in Attendances.Select(a => a.Attendee))
+            {
+                attendee.Notify(notification);
+            }
+        }
+
+        public void Modify(DateTime dateTime, string venue, byte genre)
+        {
+            Notification notification = Notification.GigUpdated(this, DateTime, Venue);
+
+            Venue = Venue;
+            DateTime = dateTime;
+            GenreId = genre;
+
+            foreach (var attendee in Attendances.Select(a => a.Attendee))
+            {
+                attendee.Notify(notification);
+            }
+        }
     }
 }
