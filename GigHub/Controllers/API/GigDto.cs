b12e10eef8d4d1f1884f31450074fe2d6f5de3c0
hasanslayer <hasan-dr2010@hotmail.com>
2019-07-12 01:28:25 +0200

    using Dtos for API controller

---
+++ GigHub/Controllers/API/GigDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using GigHub.Models;
+
+namespace GigHub.Controllers.API
+{
+    public class GigDto
+    {
+        public int Id { get; set; }
+        public bool IsCanceled { get; set; }
+        public UserDto Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }
+        public GenreDto Genre { get; set; }
+    }
+}
---
+++ GigHub/Controllers/API/NotificationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using GigHub.Models;
+
+namespace GigHub.Controllers.API
+{
+    public class NotificationDto
+    {
+        public DateTime DateTime { get; set; }
+        public NotificationType Type { get; set; }
+        public DateTime? OriginalDateTime { get; set; }
+        public string OriginalVenue { get; set; }
+        public GigDto Gig { get; set; }
+    }
+}
