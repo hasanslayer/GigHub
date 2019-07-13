37d97d4c8551e74e7c2a4919d38d8aeeab301453
hasanslayer <hasan-dr2010@hotmail.com>
2019-07-13 23:26:45 +0200

    Improve the popup look and feel and using moment.js library

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -10,6 +10,7 @@ namespace GigHub
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/moment.js",
                         "~/Scripts/underscore-min.js"
                         ));
 
