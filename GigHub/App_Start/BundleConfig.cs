71b684d53bb8f49ee0e3d9378f275c497a15045a
hasanslayer <hasan-dr2010@hotmail.com>
2019-07-13 14:39:27 +0200

    Using popup and animation for notification.

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -26,7 +26,9 @@ namespace GigHub
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/animate.css"
+                      ));
         }
     }
 }
