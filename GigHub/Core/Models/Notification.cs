aec4d255b00ec9e15690b52170612a022294c518
hasanslayer <hasan-dr2010@hotmail.com>
2019-07-16 20:20:03 +0200

    Add entity configurations

--- GigHub/Core/Models/Notification.cs
+++ GigHub/Core/Models/Notification.cs
@@ -6,12 +6,15 @@ namespace GigHub.Core.Models
     public class Notification
     {
         public int Id { get; private set; }
+
         public DateTime DateTime { get; private set; }
+
         public NotificationType Type { get; private set; }
+
         public DateTime? OriginalDateTime { get; private set; }
+
         public string OriginalVenue { get; private set; }
 
-        [Required]
         public Gig Gig { get; private set; }
 
         protected Notification()
---
+++ GigHub/Persistence/EntityConfiguration/ApplicationUserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using GigHub.Core.Models;
+
+namespace GigHub.Persistence.EntityConfiguration
+{
+    public class ApplicationUserConfiguration : EntityTypeConfiguration<ApplicationUser>
+    {
+        public ApplicationUserConfiguration()
+        {
+            Property(a => a.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            HasMany(a => a.Followers)
+                .WithRequired(a => a.Followee)
+                .WillCascadeOnDelete(false);
+
+            HasMany(a => a.Followees)
+                 .WithRequired(a => a.Follower)
+                 .WillCascadeOnDelete(false);
+
+
+
+
+        }
+    }
+}
