30dce3c5a20592bf9772b218ba834c9287ed3451
hasanslayer <hasan-dr2010@hotmail.com>
2019-07-18 18:00:51 +0200

    SetUp a project for integration test

---
+++ GigHub.IntegrationTests/GlobalSetUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GigHub.IntegrationTests
+{
+    [SetUpFixture] // NUnit attribute
+    public class GlobalSetUp
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            // need to reference configuration class for a code first migration
+
+            var configuration = new GigHub.Migrations.Configuration();
+
+            var migrator = new DbMigrator(configuration);
+
+            migrator.Update();  // if we don't have a database it will created if not then update to latest version
+        }
+    }
+}
---
+++ GigHub.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GigHub.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GigHub.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9cb8551a-008c-4baf-8be1-a54724a5f7e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
