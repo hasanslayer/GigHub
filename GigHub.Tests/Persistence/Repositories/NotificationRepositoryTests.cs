f6a32fc6770db0c71f35ed0e5c06cf76aee4921f
hasanslayer <hasan-dr2010@hotmail.com>
2019-07-18 17:32:07 +0200

    testing notification repository tests

---
+++ GigHub.Tests/Persistence/Repositories/NotificationRepositoryTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using FluentAssertions;
+using GigHub.Core.Models;
+using GigHub.Persistence;
+using GigHub.Persistence.Repositories;
+using GigHub.Tests.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GigHub.Tests.Persistence.Repositories
+{
+    [TestClass]
+    public class NotificationRepositoryTests
+    {
+        private NotificationRepository _repository;
+        private Mock<DbSet<UserNotification>> _mockNotifications;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockNotifications = new Mock<DbSet<UserNotification>>();
+
+            var mockContext = new Mock<IApplicationDbContext>();
+            mockContext.SetupGet(c => c.UserNotifications).Returns(_mockNotifications.Object);
+
+            _repository = new NotificationRepository(mockContext.Object);
+        }
+
+        [TestMethod]
+        public void GetNewNotifications_NotificationIsRead_ShouldNotBeReturned()
+        {
+            var notification = Notification.GigCanceled(new Gig());
+            var user = new ApplicationUser { Id = "1" };
+            var userNotification = new UserNotification(user, notification);
+
+            userNotification.Read();
+
+            _mockNotifications.SetSource(new[] { userNotification });
+
+            var notifications = _repository.GetNewNotifications(user.Id);
+
+            notifications.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetNewNotifications_NotificationForADifferentUser_ShouldNotBeReturned()
+        {
+            var notification = Notification.GigCanceled(new Gig());
+            var user = new ApplicationUser { Id = "1" };
+            var userNotification = new UserNotification(user, notification);
+
+            _mockNotifications.SetSource(new[] { userNotification });
+
+            var notifications = _repository.GetNewNotifications(user.Id + "-");
+
+            notifications.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void GetNewNotifications_NotificationForTheGivenUser_ShouldBeReturned() // not completed well (can't find the bug here)
+        {
+            var notification = Notification.GigCanceled(new Gig());
+            var user = new ApplicationUser { Id = "1" };
+            var userNotification = new UserNotification(user, notification);
+
+            _mockNotifications.SetSource(new[] { userNotification });
+
+            var notifications = _repository.GetNewNotifications(user.Id);
+
+            notifications.Should().HaveCount(1);
+            notifications.First().Should().Be(notification);
+        }
+    }
+}
